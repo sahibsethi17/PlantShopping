99ce2e2bad57772c4a77f270f83d15897e8d7636
Yash <sahib1701@gmail.com>
2024-09-18 15:41:03 -0400

    Created cart reducers and cartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+      let total = 0;
+      cart.state.items.forEach(item => {
+          total += (item.cost * item.quantity);
+      });
+      return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+      onContinueShopping();
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+      dispatch(updateQuantity({ item, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+      if (item.quantity === 1) {
+          dispatch(removeItem(item));
+      } else {
+          dispatch(updateQuantity({item, quantity: item.quantity - 1}));
+      }
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+      return item.cost * item.quantity;
   };
 
   return (
@@ -57,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
